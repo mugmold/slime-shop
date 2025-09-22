d141430019aeff6cb4e4a5aff0230ba3216e5173
mugmold <mugmold@gmail.com>
2025-09-22 21:30:13 +0700

    Tugas 4

--- main/views.py
+++ main/views.py
@@ -3,27 +3,46 @@ from django.core import serializers
 from django.shortcuts import get_object_or_404, redirect, render
 from main.models import Product
 from main.forms import ProductForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
+@login_required(login_url='/login')
 def home_page(request):
-    product_list = Product.objects.all()
+    filter_type = request.GET.get("filter", "all")
+    if filter_type == "all":
+        product_list = Product.objects.all()
+    else:
+        product_list = Product.objects.filter(user=request.user)
+
+    user = request.user
 
     context = {
         'npm': '2406347424',
         'name': 'Bermulya Anugrah Putra',
         'class': 'PBP D',
         'product_list': product_list,
+        "username": user.username,
+        'last_login': request.COOKIES.get('last_login', 'Never'),
     }
 
     return render(request, 'home.html', context)
 
 
+@login_required(login_url='/login')
 def create_product(request):
     form = ProductForm(request.POST or None)
 
     if request.method == "POST":
         if form.is_valid():
-            form.save()
+            product_entry = form.save(commit=False)
+            product_entry.user = request.user
+            product_entry.save()
             return redirect('main:home_page')
 
         else:
@@ -43,6 +62,7 @@ def create_product(request):
     return HttpResponse(status=404)
 
 
+@login_required(login_url='/login')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
 
@@ -53,18 +73,21 @@ def show_product(request, id):
     return render(request, "product_detail.html", context)
 
 
+@login_required(login_url='/login')
 def show_xml(request):
     product_list = Product.objects.all()
     xml_data = serializers.serialize("xml", product_list)
     return HttpResponse(xml_data, content_type="application/xml")
 
 
+@login_required(login_url='/login')
 def show_json(request):
     product_list = Product.objects.all()
     json_data = serializers.serialize("json", product_list)
     return HttpResponse(json_data, content_type="application/json")
 
 
+@login_required(login_url='/login')
 def show_xml_by_id(request, product_id):
     try:
         product_item = Product.objects.filter(pk=product_id)
@@ -74,6 +97,7 @@ def show_xml_by_id(request, product_id):
         return HttpResponse(status=404)
 
 
+@login_required(login_url='/login')
 def show_json_by_id(request, product_id):
     try:
         product_item = Product.objects.get(pk=product_id)
@@ -81,3 +105,42 @@ def show_json_by_id(request, product_id):
         return HttpResponse(json_data, content_type="application/json")
     except Product.DoesNotExist:
         return HttpResponse(status=404)
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Your account has been succesfully created!')
+            return redirect('main:login')
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:home_page"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
